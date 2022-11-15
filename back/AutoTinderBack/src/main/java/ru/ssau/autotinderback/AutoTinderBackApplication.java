2895a5baee61e194ee7aacda0551a7c0b21f0349
Ildar <ildarthegreat@gmail.com>
2022-11-15 23:42:39 +0400

    feat: init backend

---
+++ back/AutoTinderBack/src/main/java/ru/ssau/autotinderback/AutoTinderBackApplication.java
@@ -0,0 +1,13 @@
+package ru.ssau.autotinderback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTinderBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoTinderBackApplication.class, args);
+    }
+
+}
---
+++ back/AutoTinderBack/src/test/java/ru/ssau/autotinderback/AutoTinderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssau.autotinderback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTinderBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
